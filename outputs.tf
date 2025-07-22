349a34d70d49f68b7b4be89c8968bd0bd1c2b560
mjth <mjth830@gmail.com>
2025-07-22 13:11:53 +0300

    formatting files and adding '.gitignore' file

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_id" {
   description = "VPC ID"
-  value = aws_vpc.this.id
+  value       = aws_vpc.this.id
 }
 
 output "aws_subnet_private_ids" {
   description = "Private Subnets IDs"
-  value = [for subnet in aws_subnet.private : subnet.id if contains(keys(subnet), "id") ]
+  value       = [for subnet in aws_subnet.private : subnet.id if contains(keys(subnet), "id")]
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
   description = "AWS region name that will be using"
-  type = string
+  type        = string
 }
 
 variable "env" {
